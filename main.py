c6aae907a7d96a782aae7e40ca76cce681b1bbb9
tic-hack <tic.hack.boom@gmail.com>
2020-03-15 17:02:48 +0100

    CHANGE: Move and rename variables in random_behavior

--- main.py
+++ main.py
@@ -116,11 +116,11 @@ class SimpleDialogueManager:
 
     def __init__(self):
         # Emoji faces have to be encoded in unicode to be display in Telegram chat
-        self.emoji_dict = {":grinning:": "\U0001F600",
-                           ":joy:": "\U0001F602",
-                           ":rolling_laughing:": "\U0001F923",
-                           ":wink:": "\U0001F609",
-                           ":zany_face:": "\U0001F92A"}
+        self.emoji = {":grinning:": "\U0001F600",
+                      ":joy:": "\U0001F602",
+                      ":rolling_laughing:": "\U0001F923",
+                      ":wink:": "\U0001F609",
+                      ":zany_face:": "\U0001F92A"}
 
         self.article_opinion = ["So cool!", "Nice!",
                                 "Interesting! keep me in touch if you get more details on it",
@@ -129,6 +129,11 @@ class SimpleDialogueManager:
         self.party_opinion = ["I'm in!", "Party!", "Go! I bring the beers",
                               "Let's go for a party"]
 
+        self.random_proposal = ["Enjoy your day!",
+                                "Who is ready for a party?",
+                                "I'm playing CS guys! join me",
+                                "How are you guys?",
+                                "The weather is really bad!"]
 
     def random_behavior(self, question):
         """
@@ -136,13 +141,6 @@ class SimpleDialogueManager:
 
         :param question str: Command/question in the chat
         """
-
-        random_proposal = ["Enjoy your day!",
-                           "Who is ready for a party?",
-                           "I'm playing CS guys! join me",
-                           "How are you guys?",
-                           "The weather is really bad!"]
-
         probability = random.randint(1, 100)
 
         if "exia" in question.lower():
@@ -158,11 +156,11 @@ class SimpleDialogueManager:
 
         if "mdr" in question.lower() or 'lol' in question.lower():
             # return a happy emojy
-            return random.choice([self.emoji_dict[":grinning:"],
-                                  self.emoji_dict[":joy:"],
-                                  self.emoji_dict[":rolling_laughing:"]])
+            return random.choice([self.emoji[":grinning:"],
+                                  self.emoji[":joy:"],
+                                  self.emoji[":rolling_laughing:"]])
         if probability <= 5:
-            return random.choice(random_proposal)
+            return random.choice(self.random_proposal)
 
         return ""
 
