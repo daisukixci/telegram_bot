afe06a9ed31c0ae8c6ec276cc94ee17eef7535e3
daisukixci <daisuki@tuxtrooper.com>
2020-03-29 19:01:31 +0200

    Update tests

--- test_main.py
+++ test_main.py
@@ -2,8 +2,10 @@
 """
 Test for main.py
 """
+import os
 import unittest
-from main import SimpleDialogueManager
+from main import DialogueManager
+from main import get_docker_secret
 
 
 class TestMain(unittest.TestCase):
@@ -15,10 +17,22 @@ class TestMain(unittest.TestCase):
         """
         Test generate answer
         """
-        dialogue_manager = SimpleDialogueManager()
+        dialogue_manager = DialogueManager()
         self.assertEqual(
-            dialogue_manager.generate_answer("/newpoll,Test,foo1,foo2"), "send_poll"
+            dialogue_manager.generate_answer("/poll,Test,foo1,foo2"), "send_poll"
         )
+        self.assertEqual(
+            dialogue_manager.generate_answer("/mpoll,Test,foo1,foo2"), "send_mpoll"
+        )
+
+    def test_get_docker_secret(self):
+        """
+        Test get docker secret
+        """
+        os.environ["FOO"] = "FOO"
+        self.assertEqual(get_docker_secret("foo"), "FOO")
+        self.assertEqual(get_docker_secret("none"), None)
+        self.assertEqual(get_docker_secret("none", default="default"), "default")
 
 
 if __name__ == "__main__":
